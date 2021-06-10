287f918f269fb50a7117381951097d2f7a729880
naurqu <Nathan.Urquhart@sas.com>
2021-06-10 09:32:46 -0400

    AAAA

--- sas-demo/commentFileWithNewName.js
+++ sas-demo/commentFileWithNewName.js
@@ -6,5 +6,4 @@
 
 // another comment
 // asdfasdfasdf
-// new one
 
--- sas-demo/render.js
+++ sas-demo/render.js
@@ -9,6 +9,7 @@
 
 // added a comment
 // another
+// comment
 
 /**
  * Given a hero object (see data.js), this function generates a "card" showing
