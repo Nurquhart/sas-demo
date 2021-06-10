fbeae7b8546896a20b38aba2aa4e6cde9d5694fa
naurqu <Nathan.Urquhart@sas.com>
2021-06-10 11:50:44 -0400

    TTTT

--- sas-demo/data.js
+++ sas-demo/data.js
@@ -11,11 +11,7 @@
  */
 const heroicData = [{
     id: 1,
-<<<<<<< HEAD
     first: "TTTT",
-=======
-    first: "CCCC",
->>>>>>> test_v2
     last: "Stark",
     name: "Iron Man",
     img: "icons/ironman.png",
