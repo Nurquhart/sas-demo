0cf595ead332273cdf30d9077b78ea236ce342ed
naurqu <Nathan.Urquhart@sas.com>
2021-06-11 10:53:11 -0400

    BBBB

--- sas-demo/data.js
+++ sas-demo/data.js
@@ -11,11 +11,7 @@
  */
 const heroicData = [{
     id: 1,
-<<<<<<< HEAD
-    first: "AAAA",
-=======
-    first: "CCCC",
->>>>>>> newBranch
+    first: "BBBB",
     last: "Stark",
     name: "Iron Man",
     img: "icons/ironman.png",
